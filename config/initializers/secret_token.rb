08cccff44d4a231eb88add76cf4e2a9dcecbab00
Gabriel Baciu <gabriel.g.baciu@gmail.com>
2011-08-16 22:14:27 -0400

    Rails Scaffold as Second Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = 'c0e55ab58a77e8c135fcbe8dfa9fe55e0175cc50902fe184b1d57a9c14b018279172dec076069967b9ff54b72d929377b66bb31f3dd175068c425a113e54cfa9'
